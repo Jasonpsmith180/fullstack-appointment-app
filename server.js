69b9b89a3a27d15dc95fd7814af7f0147e31c8e9
cory alport <cralport@gmail.com>
2021-05-04 21:27:30 -0600

    added env file

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const routes = require("./controllers");
-const moment = require("moment");
 const Year = require("./assets/js/year.json");
 
 const sequelize = require("./config/connection");
