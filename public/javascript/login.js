a4d161b638c18ff62a1bbe7a36e88da8ce28053a
Jason <jasonpsmith180@gmail.com>
2021-05-04 21:21:20 -0600

    add views

---
+++ public/javascript/login.js
@@ -0,0 +1,63 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    // get signup info
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    console.log(username, email, password)
+
+    // if (username && email && password) {
+    //     const response = await fetch('/api/users', {
+    //         method: 'post',
+    //         body: JSON.stringify({
+    //             username,
+    //             email,
+    //             password
+    //         }),
+    //         headers: { 'Content-Type': 'application/json' }
+    //     });
+
+    //     // check response status
+    //     if (response.ok) {
+    //         console.log('success')
+    //     } else {
+    //         alert(response.statusText);
+    //     }
+    // }
+}
+
+
+
+async function loginFormHandler(event) {
+    event.preventDefault();
+
+    // get login info
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    console.log(email, password);
+
+    // if (email && password) {
+    //     const response = await fetch('/api/users/login', {
+    //         method: 'post',
+    //         body: JSON.stringify({
+    //             email,
+    //             password
+    //         }),
+    //         headers: { 'Content-Type': 'application/json' }
+    //     });
+
+    //     // check response status
+    //     if (response.ok) {
+    //         document.location.replace('/dashboard');
+    //     } else {
+    //         alert(response.statusText);
+    //     }
+    // }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
