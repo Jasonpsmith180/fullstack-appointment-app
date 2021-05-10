a4dd0dad65b93fd2e976611e5fe0a7894fc25757
robertmcshinsky <robert.mcshinsky@gmail.com>
2021-05-09 19:51:43 -0600

    Fixed label boxes

--- controllers/appointment-routes.js
+++ controllers/appointment-routes.js
@@ -1,29 +1,24 @@
-const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User } = require('../models');
-const withAuth = require('../utils/auth')
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', withAuth, (req, res) => {
-    Post.findAll({
-        where: {
-            // use session id
-            user_id: req.session.user_id
-        },
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'date_time'
-        ]
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use session id
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "content", "date_time"],
+  })
+    .then((dbPostData) => {
+      // serialize data before passing into template
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("appointment", { posts, loggedIn: true });
     })
-    .then(dbPostData => {
-        // serialize data before passing into template
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('appointment', { posts, loggedIn: true });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
